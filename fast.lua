044feb3d922bedadc73619cc3e4e8d893a846aac
marked <marked@gmail.com>
2019-09-04 17:18:03 -0500

    Initial checkin

---
+++ fast.lua
@@ -0,0 +1,14 @@
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local next_fh = assert(io.popen("jq .result.paging.next '" .. file .. "'"))
+  local next = next_fh:read('*all')
+  next = next:gsub("\"", "")
+  next = next:gsub("\n", "")
+  print(next)
+  return {
+    {
+      url="https://sketch.sonymobile.com/api/1/feed/global/list/default/"..next,
+      link_expect_html=0,
+      link_expect_css=0
+    }
+  }
+end
